28e7699e653af57a1caa20a4143bffee328b8268
Ashley Wright <ahmedtrg981106@gmail.com>
2020-09-25 17:07:49 +0300

    Add biome registry key

---
+++ src/main/java/mod/coda/croodaceouscraft/init/CroodsBiomes.java
@@ -0,0 +1,11 @@
+package mod.coda.croodaceouscraft.init;
+
+import mod.coda.croodaceouscraft.Croods;
+import net.minecraft.util.RegistryKey;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.biome.Biome;
+
+public class CroodsBiomes {
+    public static final RegistryKey<Biome> CROODACEOUS_DESERT = RegistryKey.func_240903_a_(Registry.BIOME_KEY, new ResourceLocation(Croods.MOD_ID, "croodaceous_desert"));
+}
